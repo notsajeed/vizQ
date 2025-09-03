9377ca5f3c900c152b034017301563d4258a475e
notsajeed <sajeed725333@gmail.com>
2025-09-03 13:39:54 +0530

    change path

--- src/quantumSimulator.tsx
+++ src/quantumSimulator.tsx
@@ -102,7 +102,7 @@ const QuantumSimulator: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/simulate", {
+      const response = await fetch("https://vizq.onrender.com/simulate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ n_qubits: nQubits, gates }),
